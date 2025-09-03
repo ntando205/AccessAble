e3a43b8ca6054a8a6381002c3cc8d24bb5a5932e
Ntandoyenkosi Memela <65190015+ntando205@users.noreply.github.com>
2025-09-03 22:46:02 +0000

    more

--- Backend/theapp/serializers.py
+++ Backend/theapp/serializers.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import Group, User
 from .models import *
 from rest_framework import serializers
 
-
+ 
+ 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
